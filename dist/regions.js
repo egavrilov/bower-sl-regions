58b77ed7a0ca4d380acb5e713c38b4e70bc0e751
huncode <me@huncode.com>
2015-12-03 17:58:17 +0300

    Add localstorage settings Set by event

--- dist/regions.js
+++ dist/regions.js
@@ -64,13 +64,19 @@
 	  value: true
 	});
 	exports.default = Regions;
-	/*@ngInject*/function Regions($http, $q) {
-	  var factory = {};
+	/*@ngInject*/function Regions($http, $rootScope, $q, $window) {
+	  var factory = {
+	    current: angular.fromJson($window.localStorage.getItem('sl.location'))
+	  };
+	  var defaultRegion = {
+	    region_id: '91eae2f5-b1d7-442f-bc86-c6c11c581fad',
+	    region_name: 'Москва'
+	  };
 	  var regions = undefined;
 
 	  factory.fetch = function () {
 	    return $q.all({
-	      location: factory.getLocation(),
+	      location: $q.when(factory.current || factory.getLocation()),
 	      regions: factory.getRegions()
 	    }).then(function (responses) {
 	      return responses.regions;
@@ -81,11 +87,12 @@
 	    return $q.when(factory.current || $http.get('http://api.love.sl/v1/geo/get_location/').then(function (response) {
 	      var current = response.data;
 	      if (!current) return $q.reject('Location not detected');
+	      if (!current.region_id) {
+	        current = defaultRegion;
+	      }
 
-	      factory.current = {
-	        id: current.region_id,
-	        name: current.region_name
-	      };
+	      factory.current.id = current.region_id;
+	      factory.current.name = current.region_name;
 
 	      return current;
 	    }));
@@ -103,13 +110,21 @@
 	    var region = regions.filter(function (_region) {
 	      return _region.id === id;
 	    })[0];
-	    console.log(region);
+	    console.log(id);
+
 	    if (!region) {
 	      console.warn('No region with such id');
 	      !retryFlag && console.warn('Trying to fetch');
 	      !retryFlag && factory.fetch().then(factory.setRegion.bind(null, id, true));
 	    }
-	    factory.current = region;
+
+	    factory.current.id = region.id;
+	    factory.current.name = region.name;
+
+	    $window.localStorage.setItem('sl.location', angular.toJson(factory.current));
+
+	    $rootScope.$emit('region:change', region.id);
+	    angular.element($window).trigger('angular::region::change', region);
 	  };
 
 	  return factory;
--- src/regions.factory.js
+++ src/regions.factory.js
@@ -1,48 +1,63 @@
-export default /*@ngInject*/ function Regions ($http, $q) {
-    let factory = {};
-    let regions;
+export default /*@ngInject*/ function Regions($http, $rootScope, $q, $window) {
+  let factory = {
+    current: angular.fromJson($window.localStorage.getItem('sl.location'))
+  };
+  const defaultRegion = {
+    region_id: '91eae2f5-b1d7-442f-bc86-c6c11c581fad',
+    region_name: 'Москва'
+  };
+  let regions;
 
-    factory.fetch = function () {
-      return $q.all({
-        location: factory.getLocation(),
-        regions: factory.getRegions()
-      }).then(function (responses) {
-        return responses.regions;
-      })
-    };
+  factory.fetch = function () {
+    return $q.all({
+      location: $q.when(factory.current || factory.getLocation()),
+      regions: factory.getRegions()
+    }).then(function (responses) {
+      return responses.regions;
+    })
+  };
 
-    factory.getLocation = function () {
-      return $q.when(factory.current || $http.get('http://api.love.sl/v1/geo/get_location/').then(function (response) {
-        const current = response.data;
+  factory.getLocation = function () {
+    return $q.when(factory.current || $http.get('http://api.love.sl/v1/geo/get_location/').then(function (response) {
+        let current = response.data;
         if (!current) return $q.reject('Location not detected');
+        if (!current.region_id) {
+          current = defaultRegion;
+        }
 
-        factory.current = {
-          id: current.region_id,
-          name: current.region_name
-        };
+        factory.current.id = current.region_id;
+        factory.current.name =current.region_name;
 
         return current;
       }));
-    };
+  };
 
-    factory.getRegions = function () {
-      return $q.when(regions || $http.get('http://api.love.sl/v2/outlets/regions/').then(function (response) {
+  factory.getRegions = function () {
+    return $q.when(regions || $http.get('http://api.love.sl/v2/outlets/regions/').then(function (response) {
         if (!response.data) return $q.reject('No regions listed');
         regions = response.data;
         return regions;
       }));
-    };
+  };
 
-    factory.setRegion = function (id, retryFlag) {
-      let region = regions.filter((_region) => _region.id === id)[0];
-      console.log(region);
-      if (!region) {
-        console.warn('No region with such id');
-        !retryFlag && console.warn('Trying to fetch');
-        !retryFlag && factory.fetch().then(factory.setRegion.bind(null, id, true));
-      }
-      factory.current = region;
-    };
+  factory.setRegion = function (id, retryFlag) {
+    let region = regions.filter((_region) => _region.id === id)[0];
+    console.log(id);
 
-    return factory;
-  }
+    if (!region) {
+      console.warn('No region with such id');
+      !retryFlag && console.warn('Trying to fetch');
+      !retryFlag && factory.fetch().then(factory.setRegion.bind(null, id, true));
+    }
+
+    factory.current.id = region.id;
+    factory.current.name = region.name;
+
+    $window.localStorage.setItem('sl.location', angular.toJson(factory.current));
+
+    $rootScope.$emit('region:change', region.id);
+    angular.element($window).trigger('angular::region::change', region);
+  };
+
+  return factory;
+}
